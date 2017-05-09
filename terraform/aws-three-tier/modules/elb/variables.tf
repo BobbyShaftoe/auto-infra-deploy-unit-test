4730d701b703470c745140cb0ef8145401d5ef1c
BobbyShaftoe <tech@halcyonnetworks.com.au>
2017-05-09 15:45:05 +0800

    Modified Terraform elb instances and sg variables type in variables.tf

--- terraform/aws-three-tier/modules/elb/variables.tf
+++ terraform/aws-three-tier/modules/elb/variables.tf
@@ -1,6 +1,10 @@
 
-variable "security_groups" {}
-variable "instances" {}
+variable "security_groups" {
+  type = list
+}
+variable "instances" {
+  type = list
+}
 
 variable "subnet_cidrs" {
   type = "list"
